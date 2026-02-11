40de80e371c094758aede9c7756bbaa0c9c613e4
Roudranil Das <Roudranil.Das@Kantar.com>
2026-02-11 21:54:34 +0530

    add fs identifier and virtual mode=True for filesystembackend

--- src/backends/custom.py
+++ src/backends/custom.py
@@ -10,18 +10,19 @@ from langchain.tools import ToolRuntime
 
 class CustomBackend:
     def __init__(self):
-        self.memories_fs = TempFS()
-        self.artifacts_fs = TempFS()
+        self.memories_fs = TempFS("memories")
+        self.artifacts_fs = TempFS("artifacts")
 
     def __call__(self, rt: ToolRuntime) -> BackendFactory:
         return CompositeBackend(
             default=StateBackend(rt),
             routes={
+                "/notes/": StateBackend(rt),
                 "/memories/": FilesystemBackend(
-                    root_dir=self.memories_fs.getsyspath("/")
+                    root_dir=self.memories_fs.getsyspath("/"), virtual_mode=True
                 ),
                 "/artifacts/": FilesystemBackend(
-                    root_dir=self.artifacts_fs.getsyspath("/")
+                    root_dir=self.artifacts_fs.getsyspath("/"), virtual_mode=True
                 ),
             },
         )
